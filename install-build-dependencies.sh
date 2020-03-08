d8f132e504e2b5f45bc47ab44719b4f3cdfc716d
Wintermute0110 <wintermute0110@gmail.com>
2020-03-09 00:03:15 +0900

    Initial commit.

---
+++ install-build-dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Installs build dependencies for ES.
+
+apt install \
+    libsdl2-dev \
+    libsdl2-mixer-dev \
+    libfreeimage-dev \
+    libfreetype6-dev \
+    libcurl4-openssl-dev \
+    libasound2-dev \
+    libgl1-mesa-dev \
+    libzip-dev \
+    build-essential \
+    git \
+    cmake \
+    fonts-droid-fallback
