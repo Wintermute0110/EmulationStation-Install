2ef756d48ff57ea8f5cfc209053ea423574a5237
Wintermute0110 <wintermute0110@gmail.com>
2020-03-09 19:32:13 +0900

    Plans for the future.

---
+++ convert_AML_Favourites_to_ES_DB.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python -B
+# -*- coding: utf-8 -*-
+
+# Converts the AML Favourites into EmulationStation DB.
+
+# Copyright (c) 2020 Wintermute0110 <wintermute0110@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+# --- Python standard library ---
+from __future__ import unicode_literals
+
+# --- Configuration ------------------------------------------------------------------------------
+
+# --- Functions ----------------------------------------------------------------------------------
+
+# --- Main ---------------------------------------------------------------------------------------
